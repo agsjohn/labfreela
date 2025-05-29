3e5f9e5d7486ebc727d008963d2bd2c5445bb80c
agsjohn <joaovitormds03@gmail.com>
2025-05-29 18:17:26 -0300

    create project

---
+++ src/main/java/com/agsjohn/labfreela/LabFreelaApplication.java
@@ -0,0 +1,13 @@
+package com.agsjohn.labfreela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabFreelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabFreelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agsjohn/labfreela/LabFreelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agsjohn.labfreela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabFreelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
